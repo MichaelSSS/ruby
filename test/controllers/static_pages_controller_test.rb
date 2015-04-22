06252647d5274713bb01fca3bcf83bc4eeb53432
mgrov <mgrov@mwmoskva.ru>
2015-04-22 22:00:19 +0400

    exercises

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,22 +1,31 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @title = ' | HopeApp'
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select 'title' , 'HOME | HopeApp'
+    assert_select 'title' , "HOME#{@title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select 'title' , 'Help | HopeApp'
+    assert_select 'title' , "Help#{@title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select 'title' , 'About | HopeApp'
+    assert_select 'title' , "About#{@title}"
   end
-  
+
+  test "should get contacts" do
+    get :contacts
+    assert_response :success
+    assert_select 'title' , "Contacts#{@title}"
+  end  
 end
