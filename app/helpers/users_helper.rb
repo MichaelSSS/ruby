cf3a4852b579af12a9a04baf3f0e39c2fd904170
MichaelSSS <mgrov@mwmoskva.ru>
2015-04-25 15:42:15 +0400

    done

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,8 @@
 module UsersHelper
-  def gravatar_for (user)
+  def gravatar_for (user, options = {size: 80})
     id = Digest::MD5::hexdigest(user.email.downcase)
-    url = "https://secure.gravatar.com/avatar/#{id}"
+    size = options[:size]
+    url = "https://secure.gravatar.com/avatar/#{id}?s=#{size}"
     image_tag url, alt: user.name, class: 'gravatar'
     
   end
--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -6,7 +6,7 @@ class UserSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_template 'users/new'
     assert_no_difference 'User.count' do
-      post_via_redirect users_path, user: { name:  "",
+      post users_path, user: { name:  "",
                                email: "user@invalid",
                                password:              "foo",
                                password_confirmation: "bar" }
@@ -24,5 +24,34 @@ class UserSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "foobar" }
     end
     assert_template 'users/show'
+  end
+  
+  test 'error messages' do
+    get signup_path
+    assert_template 'users/new'
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_select '#error_explanation'
+    assert_select '.alert-danger'
+    assert_select '#error_explanation > ul > li:nth-child(2)', 'Email is invalid'
+  end
+  
+  test "successful signup flash" do
+    get signup_path
+    assert_template 'users/new'
+    assert_difference 'User.count' do
+      post_via_redirect users_path, user: { name:  "Nnn Fff",
+                               email: "user@valid.mail",
+                               password:              "foobar",
+                               password_confirmation: "foobar" }
+    end
+    assert_template 'users/show'
+    assert_not flash.empty?
+    assert_select 'div.alert-success'
+    assert_select 'div.alert-success', 1
   end
 end
