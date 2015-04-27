231d6bd7876bbf89eb28e53540c36a799a183a01
MichaelSSS <mgrov@mwmoskva.ru>
2015-04-27 20:20:26 +0400

    add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,9 +13,20 @@ class User < ActiveRecord::Base
   validates :password, length: {minimum: 6}, allow_blank: true
   
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships#, source: :follower
   
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships
+                     WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+                     OR user_id = :user_id", user_id: id)
   end
   
   def User.digest(string)
@@ -61,9 +72,23 @@ class User < ActiveRecord::Base
     update_attribute(:reset_digest,  User.digest(reset_token))
     update_attribute(:reset_sent_at, Time.zone.now)
   end
+  
   def send_password_reset_email
     UserMailer.password_reset(self).deliver
   end
+  
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+  
   private 
     def create_activation_digest
       self.activation_token = User.new_token
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,12 @@ users = User.order(:created_at).take(2)
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[1..5]
+followers = users[3..4]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
